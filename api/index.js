6fe5d67299bd53e536752d06aa838420b413a2db
Hope Shirley <77952833+hopeminyoso@users.noreply.github.com>
2023-11-08 11:05:34 +0300

    connect to database

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // This loads the .env file into process.env
+
+const app = express();
+
+// Connecting to MongoDB using Mongoose and process.env
+mongoose.connect(process.env.MONGODB_URI, {
+ 
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
