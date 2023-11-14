cab54b88ed4525a0e26922163c6754a763b4ee07
Hope Shirley <77952833+hopeminyoso@users.noreply.github.com>
2023-11-14 14:50:48 +0300

    complete sign up page ui

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+        />
+
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
